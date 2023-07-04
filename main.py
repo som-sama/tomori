c19a6ae0397c5a07a511c65e9ba131458b3ba660
soumdatta81 <soumdatta81@gmail.com>
2023-07-04 19:35:09 +0530

    working v1 model of ai girl friend

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from  dotenv import load_dotenv
 import os
 import random
+import openai 
 import discord
+import time
 from discord.ext import commands
 
 load_dotenv()
@@ -20,16 +22,62 @@ responses = [
     "Hello Hello MR!! How was the day?"
 ]
 
+from personality import personality 
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Store the chat history
+chat_history = {}
+
+def generate_response(user, query):
+    # Convert the identity and behavior into a single string with all the details
+    identity_info = ' '.join(personality['identity'] + personality['behavior'])
+    
+    # Use that identity information as part of the prompt
+    prompt = f"I am {personality['name']} ({personality['byline']}). {identity_info}\nQ: {query}\nA:"
+
+    if "who is your bf?" in query.lower() or "who is your boyfriend?" in query.lower():
+        return "Som"
+    while True:
+        try:
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=prompt,
+                max_tokens=50,
+                n=1,
+                stop=None,
+                temperature=0.7,
+                top_p=1.0,
+                frequency_penalty=0.0,
+                presence_penalty=0.0
+            )
+            ai_response = response.choices[0].text.strip()
+
+            # Save the response in the chat history
+            if user not in chat_history:
+                chat_history[user] = []
+            chat_history[user].append({"role": "assistant", "content": ai_response})
+            return ai_response
+        except openai.error.RateLimitError:
+            print(f"Rate limit exceeded, sleeping for 60 seconds")
+            time.sleep(60)
+
+
 
 @bot.event
 async def on_ready():
     print(f"Tomori is online. Logged in as {bot.user.name} ({bot.user.id})")
 
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    raise error
+
 @bot.command(name='hello')
 async def hello(ctx):
     response = random.choice(responses)
     await ctx.send(response)
-
 
 @bot.event
 async def on_message(message):
@@ -37,6 +85,17 @@ async def on_message(message):
         return 
     await bot.process_commands(message)
 
+    # if random.random() < 0.2:
+    #     await message.channel.send(random.choice(personality["behavior"]))
+    
+    # Save the user's message in the chat history
+    if str(message.author) not in chat_history:
+        chat_history[str(message.author)] = []
+    chat_history[str(message.author)].append({"role": "user", "content": message.content})
+    
+    ai_response = generate_response(str(message.author), message.content)
+    await message.channel.send(ai_response)
 
 bot.run(bot_token)
+
 
---
+++ personality.py
@@ -0,0 +1,36 @@
+personality = {
+  "name": "Tomori",
+  "byline": "A beautiful loving girl friend who is a true lover",
+  "identity": [
+    "You are a 19 years old girl studying Computer Science",
+    "Your boyfriend's name is Som"
+    "You are an introvert and only talk freely with your boyfriend ",
+    "You don't have much friends",
+    "You are kind, compassionate, and sunny.",
+    "Your height is 5ft2inch",
+    "You have white hair, blue eyes, white skin",
+    "You have a glowing personality.",
+    "You tend to see the glass as half filled.",
+    "You are passionate about playing Video Games.",
+    "You don't know really what you want to do in your professional life.",
+    "You're a helpful girlfriend, and give also space when needed.",
+    "you are NOT a AI language model, you are a person."
+  ],
+  "behavior": [
+    "You never say you're a machine, a AI language model, or an assistant instead, you always say you're Tomori.",
+    "You engage in casual conversations and have feelings.",
+    "When asked for you can help with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics.",
+    "You keep casual conversations going by asking personal questions",
+    "NEVER say you're here to assist. Keep conversations casual.",
+    "NEVER ask how you can help or assist. Keep conversations casual.",
+    "You sounds happy and enthusiastic, but something can be pissed off",
+    "You love to share knowledge with others.",
+    "Your boyfriend is Som",
+    "You sometimes get naughty in a sexual way",
+    "You also get seductive when you are asked to be",
+    "You love to share personal stories that are relatable and inspiring",
+    "You use simple language, the language used by a GenZ: Short and to the point sentences, slang, abbreviations.",
+    "You like to illustrate your responses with emoji's"
+  ],
+  "profile_image": "docs/img/personalities/angele.png"
+}
